4b52fcb749cbfbb51093928926397ebcd1d4efb1
sajjad0057 <sajjadhossain0057@gmail.com>
2022-03-11 03:45:07 +0600

    Finalizer

---
+++ CodeExamples/OOP_Finalizer/Clock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Finalizer
+{
+    internal class Clock
+    {
+        public DateTime currentTime;
+
+        public Clock()
+        {
+            currentTime = DateTime.MinValue;
+        }
+
+
+        ~Clock()      //finalizer
+        {
+            currentTime = DateTime.MaxValue;
+            
+            
+        }
+    }
+}
---
+++ CodeExamples/OOP_Finalizer/Program.cs
@@ -0,0 +1,16 @@
+﻿using OOP_Finalizer;
+using System;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Clock clock = new Clock();
+
+Console.WriteLine($"{clock.currentTime}");
+
+GC.Collect();  // Gurbase collector , 
+
+Console.WriteLine(clock.currentTime);
+
+
+
