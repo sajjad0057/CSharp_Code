ec65ce7e87871cf2e604a22cbfca91fdf7b5c76e
sajjad0057 <sajjadhossain0057@gmail.com>
2022-03-25 01:06:35 +0600

    try to understand string and StringBuilder

---
+++ CodeExamples/StringBuilder_Example/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+
+//string ? x = Console.ReadLine();  // by this if we don't assing string in x; x set default null value . 
+//Console.WriteLine(x);
+
+
+string text = "Hello";
+
+string text2 = "World";
+
+string text3 = text + text2;
+
+//for (int i = 0; i < text.Length; i++)
+//{
+//    if (text[i] == 'a' || text[i] == 'b')
+//        text[i] = '_';     // here not possible modifying in string conditionally . 
+//}
+
+
+
+
+
+/*
+// if we use this , every string concatenation create a new object so that this way kill memory more 
+string fulltext = string.Empty;
+
+while (true)
+{
+    var line = Console.ReadLine();
+    if (line == "")
+        break;
+    fulltext += line;
+}
+ 
+ */
+
+
+
+// efficient way to modifying string using StringBuilder, that not create new object for every modification . 
+
+StringBuilder fulltext = new StringBuilder(); 
+
+while (true)
+{
+    var line = Console.ReadLine();  
+    if (line == "")
+        break;
+    fulltext.Append(line);
+}
+
+
+Console.WriteLine(fulltext);
