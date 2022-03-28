cfcc2609b2e7988623c30e0ef0779bc22ffa11a5
sajjad0057 <sajjadhossain0057@gmail.com>
2022-03-29 02:19:09 +0600

    practice anonymous objects and object initializers

---
+++ CodeExamples/Anonymous_Object/Cat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_Object
+{
+    public class Cat
+    {
+        // Auto-implemented properties.
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Cat()
+        {
+
+        }
+
+        public Cat(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ CodeExamples/Anonymous_Object/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using Anonymous_Object;
+
+/*
+ Example of anonymous functions : 
+ */
+
+var o = new { Name = "sajjad", Address = "Rangpur" };
+
+
+
+// or 
+
+object  a = new { Name = "sajjad1", Address = "Pabna" };
+
+dynamic x = a;
+
+Console.WriteLine($"Name : {o.Name} ; Address : {o.Address}");
+
+Console.WriteLine($"Name : {x.Name} ; Address : {x.Address}");
+
+
+
+/*
+ Object initializers :
+ By using object initalizers , we - assign values to any accessible fields or properties of an object at creation
+ time without having to invoke a constructor followed by lines of assignment statements.
+
+Example given below : 
+
+ */
+
+
+
+Cat cat = new Cat { Age = 10, Name = "Fluffy" };
+Cat sameCat = new Cat("Fluffy") { Age = 10 };  // initialize methods here at a time . 
+
+Console.WriteLine(cat.Name);
+Console.WriteLine(sameCat.Age);
+
