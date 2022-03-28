092cf5c93d5f935f34d5c95d8cbc61bbc02fbb7f
sajjad0057 <sajjadhossain0057@gmail.com>
2022-03-29 03:02:23 +0600

    uses generic datatype in methods

--- CodeExamples/Generics_Examples/Program.cs
+++ CodeExamples/Generics_Examples/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+public struct Coordinate<T,Q>    // here T, Q data type  , passed as parameter . 
+{
+    public T X { get; set; }
+    public  Q Y { get; set; }
+
+
+    // using generic typed in methods
+
+    public T DoSomething(Q q)  // parameter type Q , that Passes in Struct 
+    {
+        throw new NotImplementedException();
+
+        // return type T .
+    }
+
+}
+
+
+public class TestCode
+{
+    public static void Main(string[] args)
+    {
+        Coordinate<int,double> intCoordinate = new Coordinate<int,double>();
+
+        intCoordinate.X = 12;
+        intCoordinate.Y = 13.0;
+
+        Coordinate<double,string> doubleCoordinate = new Coordinate<double,string> { X = 5.5, Y = "sajjad" }; // object initializer . 
+    }
+}
