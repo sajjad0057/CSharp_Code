73a588a6b32c110658e2746b26cfe6cae98a8bc1
sajjad0057 <sajjadhossain0057@gmail.com>
2022-04-06 01:13:16 +0600

    Practice Clone one object from another object using normal method

--- CodeExamples/Important_BuiltIn_Interface/Program.cs
+++ CodeExamples/Important_BuiltIn_Interface/Program.cs
@@ -66,3 +66,34 @@ IEnumerable<object> something = new List<object>();
 //Box b = (Box)something;   // if we use casting or Boxing / UnBoxing is this case throw an error 
 
 Box b2 = something as Box;   // here b2 set  null value  , coz, something is not Box, But does not throw an Error . 
+
+
+Console.WriteLine("====================================================================");
+
+/*
+ * 
+The ICloneable interface enables you to provide a customized implementation that creates a copy of an existing object.
+
+*/
+
+Console.WriteLine("Practice IColoneable interface");
+
+
+// here we can say bottle1 and his property is a prototype of WaterBottle Class . 
+
+WaterBottle bottle1 = new WaterBottle(100,50,"blue");
+bottle1.ReleaseWater(10);
+bottle1.ReleaseWater(15);
+bottle1.AddWater(20);
+
+Console.WriteLine($"bottle1 :: Capacity : {bottle1.Capacity} ; WaterAmount : {bottle1.WaterAmount} ; Color : {bottle1.Color} ");
+
+
+// copying bottle1 prototype we can create same as bottle1, another object from  that have same property, by this given way : 
+
+WaterBottle bottle2 = bottle1.Copy();
+WaterBottle bottle3 = bottle1.Copy();
+WaterBottle bottle4 = bottle1.Copy();
+
+Console.WriteLine($"bottle2 :: Capacity : {bottle2.Capacity} ; WaterAmount : {bottle2.WaterAmount} ; Color : {bottle2.Color} ");
+Console.WriteLine($"bottle4 :: Capacity : {bottle4.Capacity} ; WaterAmount : {bottle4.WaterAmount} ; Color : {bottle4.Color} ");
---
+++ CodeExamples/Important_BuiltIn_Interface/WaterBottle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Important_BuiltIn_Interface
+{
+    public class WaterBottle
+    {
+        public int Capacity { get;private set; }
+
+        public int WaterAmount { get;private set; }
+
+        public string Color { get;private set; }
+
+        public WaterBottle(int capacity,int waterAmount , string color)
+        {
+            Capacity = capacity;
+            WaterAmount = waterAmount;
+            Color = color;
+        }
+
+        public void ReleaseWater(int amount)
+        {
+            WaterAmount -= amount;
+        }
+
+        public void AddWater(int amount)
+        {
+            if(WaterAmount < Capacity)
+                WaterAmount += amount;
+        }
+
+
+        // this method can be used for copying this class object 
+        public WaterBottle Copy()
+        {
+            return new WaterBottle(Capacity,WaterAmount,Color);
+        }
+
+    }
+}
