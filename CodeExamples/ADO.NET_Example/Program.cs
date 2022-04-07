8ee9f336d2b5a1682106ab846181483041b39631
sajjad0057 <sajjadhossain0057@gmail.com>
2022-04-07 10:58:45 +0600

    practicing write reuseable code - 4 (practice date retrive operation using ExecuteReader with ADO.NET and arrange data with generic collections)

--- CodeExamples/ADO.NET_Example/Program.cs
+++ CodeExamples/ADO.NET_Example/Program.cs
@@ -5,9 +5,13 @@ using ADO.NET_Example;
 string connectionString = "Server = DESKTOP-SU7UN5F\\SQLEXPRESS ; Database=CSharpPractice1 ; User Id = CSharpPractice1 ;Password = 123456;";
 DataUtility dataUtility = new DataUtility(connectionString);
 
-var sql = "update Students set Cgpa = 3.94 where id = 4";
+
+//var sql = "insert into Students([Name],Cgpa,DateOfBirth) values('sakib',3.58,'2002-07-11')";
+//var sql = "update Students set Cgpa = 3.94 where id = 4";
 //var sql = "delete from Students where id = 4";
 //dataUtility.ExecuteCommand(sql);
+
+//var query = "Select Name,DateOfBirth from Students";
 
 var query = "Select * from Students";
 
