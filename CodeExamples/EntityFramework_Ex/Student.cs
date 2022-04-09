2662ec7aeb96ddfb7846cc1d5274fecfa64de2d2
sajjad0057 <sajjadhossain0057@gmail.com>
2022-04-10 04:34:38 +0600

    working procedure of Entity framework -1

---
+++ CodeExamples/EntityFramework_Ex/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework_Ex
+{
+    public class Student    // here Student Class is called Entity , By this class In Entity Framework will make a table in database .
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Cgpa { get; set; }
+        public DateTime DateOfBirth { get; set; }
+       
+    }
+}
+ 
---
+++ CodeExamples/EntityFramework_Ex/TestDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework_Ex
+{
+    public class TestDbContext : DbContext       // here inherit DbContext base class class from Microsoft.EntityFrameworkCore
+    {
+        private string _connectionString;
+
+        private string _assemblyName;
+        public DbSet<Student> Students { get; set; }     //using DbSet<Student> generic class  here, Student class connect with DbContext class . 
+
+        public TestDbContext()
+        {
+            _connectionString = "Server = DESKTOP - SU7UN5F\\SQLEXPRESS; Database = CSharpPractice1; User Id = CSharpPractice1; Password = 123456;";
+            /*
+                Assembly.GetExecutingAssembly() is .NET method from Reflection namespace , thats provides static methods.
+                it's Assembly of running project . Assembly.GetExecutingAssembly().FullName provide here FullName of Assembly .
+                Assembly means .exe/.dll file that's contain in project 
+            */
+            _assemblyName = Assembly.GetExecutingAssembly().FullName;
+
+            Console.WriteLine($"Assembly.GetExecutingAssembly().FullName : {Assembly.GetExecutingAssembly().FullName}");
+        }
+
+        /*
+            OnConfiguring method configuring database connection with Entity Framework .
+        */
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            if (!dbContextOptionsBuilder.IsConfigured)
+            {
+                dbContextOptionsBuilder.UseSqlServer(_connectionString,m=>m.MigrationsAssembly(_assemblyName));
+            }
+            base.OnConfiguring(dbContextOptionsBuilder); // overide some code from base class .  
+        } 
+    }
+}
