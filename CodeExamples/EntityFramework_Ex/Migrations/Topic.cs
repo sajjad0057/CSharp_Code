4fe1313e74d559b1faddb75bbf967fe97610b783
sajjad0057 <sajjadhossain0057@gmail.com>
2022-04-14 07:45:13 +0600

    create topic entity

---
+++ CodeExamples/EntityFramework_Ex/Migrations/Topic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework_Ex.Migrations
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public string Detail { get; set; }
+    }
+}
