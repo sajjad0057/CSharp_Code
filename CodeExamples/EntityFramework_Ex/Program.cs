8983ed425fee38b10d63c29abf53456a8bc35b23
sajjad0057 <sajjadhossain0057@gmail.com>
2022-04-14 07:26:56 +0600

    add another entity Course

--- CodeExamples/EntityFramework_Ex/Program.cs
+++ CodeExamples/EntityFramework_Ex/Program.cs
@@ -1,11 +1,11 @@
-﻿
-// insert data in table Student 
-
-using EntityFramework_Ex;
-
-Student student1 = new Student { Name = "sajjad", Cgpa = 3.2, Address = "Rangpur" , DateOfBirth = new DateTime(1991,03,06) };   
+﻿using EntityFramework_Ex;
 
 TestDbContext context = new TestDbContext();
+
+#region Insert Data in DB
+
+// insert data in table Student 
+Student student1 = new Student { Name = "sajjad", Cgpa = 3.2, Address = "Rangpur", DateOfBirth = new DateTime(1991, 03, 06) };
 
 // way 1  
 
@@ -13,17 +13,22 @@ TestDbContext context = new TestDbContext();
 
 // way 2
 
-
 //context.Students.Add(new Student { Name = "sakib", Cgpa = 3.05, DateOfBirth = new DateTime(2002, 05, 09), Address = "Dhaka" });
 
 //context.Students.Add(new Student { Name = "sajjad", Cgpa = 3.20, DateOfBirth = new DateTime(2002, 05, 09), Address = "Rangpur" });
 
 context.Students.Add(new Student { Name = "zahan", Cgpa = 3.55, DateOfBirth = new DateTime(1998, 05, 09), Address = "pabna" });
 
-context.SaveChanges();
+//context.SaveChanges();
+
+#endregion 
 
 
 
+
+
+
+#region Retrive and Update Data 
 
 //Retriving and update Data from Database :
 
@@ -32,6 +37,11 @@ Student student2 = context.Students.Where(x => x.Name == "sakib").FirstOrDefault
 student2.Cgpa = 3.90;
 
 context.SaveChanges();
+
+#endregion
+
+
+#region Delete or Remove Data 
 
 // delete or remove data from data table 
 
@@ -42,6 +52,11 @@ context.SaveChanges();
 //}
 
 
+
+#endregion 
+
+
+#region  Retriving Data as a List 
 
 // Get all data as a list :
 
@@ -58,6 +73,8 @@ foreach (Student student in studentList)
 {
     Console.WriteLine($"Name : {student.Name} ; Cgpa : {student.Cgpa} ; Address : {student.Address} ; DateOfBirth : {student.DateOfBirth}");
 }
+
+#endregion
 
 
 
