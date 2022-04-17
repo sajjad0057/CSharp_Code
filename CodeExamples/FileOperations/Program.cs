8dc1765116de92cf822cdb57be7ea987091c2df1
sajjad0057 <sajjadhossain0057@gmail.com>
2022-04-17 17:57:35 +0600

    into to work with File operations in c#

---
+++ CodeExamples/FileOperations/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+// File
+// Directory 
+
+
+// FileInfo 
+
+// DirectoryInfo
+
+
+string t = "hello \"world";
+
+
+// check exists file or not :
+
+if (File.Exists(@"F:\CSharp_Code\CodeExamples\FileOperations\hello.txt"))
+    //if exists file , then delete this file :
+    File.Delete(@"F:\CSharp_Code\CodeExamples\FileOperations\hello.txt");
+else
+    Console.WriteLine("File doesn't exists !");
+
+
+
+
+
+
+
