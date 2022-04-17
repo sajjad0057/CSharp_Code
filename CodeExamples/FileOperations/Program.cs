eca5131379bf5056e18c5a0814dea3b0bb256939
sajjad0057 <sajjadhossain0057@gmail.com>
2022-04-18 00:23:48 +0600

    work with FileInfo object for performing various file operations

--- CodeExamples/FileOperations/Program.cs
+++ CodeExamples/FileOperations/Program.cs
@@ -1,25 +1,31 @@
-﻿
+﻿using System.Text;
+using System.IO;
 
-// File
-// Directory 
+/*
+------------------------------------------------------------------------------------------------
+ Stream is One type of Connection Object. 
+------------------------------------------------------------------------------------------------
 
+ File
+ Directory 
 
-// FileInfo 
-
-// DirectoryInfo
+ FileInfo 
+ DirectoryInfo
+ 
+ */
 
 /*
  * see given link for more info about File Operations in C# : 
  * 
 https://www.c-sharpcorner.com/UploadFile/puranindia/file-handling-in-C-Sharp/ 
-*************Learn About More Files and Directories in C# see Given Link : **********
+----------------------------------------------------------------------------------------------
+********** Learn About More Files and Directories in C# see Given Link : **********
+----------------------------------------------------------------------------------------------
 https://www.c-sharpcorner.com/UploadFile/rohatash/working-with-the-fileinfo-class-in-C-Sharp/
 */
 
-string t = "hello \"world";
-
-
-// check exists file or not :
+#region  check file exists or not / and How delete File :
+// check file exists or not :
 
 if (File.Exists(@"F:\CSharp_Code\CodeExamples\FileOperations\hello.txt"))
 {
@@ -29,17 +35,71 @@ if (File.Exists(@"F:\CSharp_Code\CodeExamples\FileOperations\hello.txt"))
 else
     Console.WriteLine("File doesn't exists !");
 
+#endregion
 
-
-
+#region Create file and Append text 
 // Creating new file in using File.Create(Path/fileName.extension); 
 
 //File.Create(@"F:\CSharp_Code\CodeExamples\FileOperations\hello.txt");
 
 
-using StreamWriter writer = File.AppendText(@"F:\CSharp_Code\CodeExamples\FileOperations\hello.txt");  // here using statement is dispose StreanWriter after finish this task . 
+//using StreamWriter writer = File.AppendText(@"F:\CSharp_Code\CodeExamples\FileOperations\hello.txt");  // here using statement is dispose StreanWriter after finish this task . 
 
-writer.WriteLine("Hello World");
+//writer.WriteLine("Hello World");
+
+#endregion
+
+
+#region Open file with verious Mode
+/*
+Stream is One type of Connection Object. FileStream  It is used to read from and write to any location within a file .  
+*/
+
+//using (FileStream fileStream = File.Open(@"F:\CSharp_Code\CodeExamples\FileOperations\hello.txt", FileMode.Append))
+//{
+//    // return a file Stream obejct  
+
+//    string text = "This is Sajjad, I want to be a well software engineer";
+
+//    byte[] data = Encoding.UTF8.GetBytes(text);            // By this convert text string to byte Array.Cz FileStream Object expect byte type data, not allow plain text 
+
+//    //fileStream.Write(data);                             // write all text
+
+//    fileStream.Write(data, 4, 10);                        // set offset, we can write specific portion of text. 
+//};
+
+#endregion
+
+#region Copy One file to another place
+
+//string sourceFilePath = @"F:\CSharp_Code\CodeExamples\FileOperations\hello.txt";
+
+//string destinationFilePath = @"F:\CSharp_Code\CodeExamples\FileOperations\world2.txt";
+
+// // static way to Copy file to another one :
+
+//File.Copy(sourceFilePath, destinationFilePath);
+
+#endregion
+
+#region  Perform File Operation using FileInfo() object ( It's using (Object) better than static approach ).
+
+// //Another way ( By creating object ) to Copy file to another one . we can aslo do all File Operation by creating FileInfo() Object. 
+
+//string sourceFilePath = @"F:\CSharp_Code\CodeExamples\FileOperations\hello.txt";
+
+////string destinationFilePath = @"F:\CSharp_Code\CodeExamples\FileOperations\world2.txt";
+
+//FileInfo fileInfo = new FileInfo(sourceFilePath);
+//if (fileInfo.Exists)
+//{
+//    fileInfo.Delete();
+//}
+
+#endregion
+
+
+
 
 
 
