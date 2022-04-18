bde3f28b5fcca188e23ec522b25905827c2bc96b
sajjad0057 <sajjadhossain0057@gmail.com>
2022-04-18 21:53:40 +0600

    practice ADO.NET for exam2

---
+++ CodeExamples/Prectice_ADO.NET/DataManageable.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prectice_ADO.NET
+{
+    public class DataManageable
+    {
+        private string _connectionString;
+
+        public DataManageable(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+
+        private SqlCommand _createCommand(string sql)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            SqlCommand cmd = new SqlCommand(sql, connection);
+
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+
+            return cmd;
+        }
+
+
+        public void ExecuteCommand(string sql)
+        {
+            using SqlCommand cmd = _createCommand(sql);  
+            cmd.ExecuteNonQuery();
+        }
+
+
+        public List<Dictionary<string, object>> ExecuteQuery(string query)
+        {
+            using SqlCommand cmd = _createCommand(query);
+
+            List<Dictionary<string,object>> values = new List<Dictionary<string, object>>();
+
+            using SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Dictionary<string,object> value = new Dictionary<string,object>();
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    value.Add(reader.GetName(i),reader.GetValue(i));
+                }
+                values.Add(value);
+            }
+            return values;
+
+        }
+
+    }
+}
--- CodeExamples/Prectice_ADO.NET/Program.cs
+++ CodeExamples/Prectice_ADO.NET/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Practice ADO.NET");
+﻿using Prectice_ADO.NET;
+
+string connectionString = "Server = DESKTOP-SU7UN5F\\SQLEXPRESS ; Database= PracticeForExam2 ; User Id = CSharpPractice1 ;Password = 123456;";
+DataManageable dataManage = new DataManageable(connectionString);
+
+
+#region Execute command  for Create,Update or Delete Data 
+
+//string sql = "insert into Students([Name],Cgpa,Dob) values('nafiul',3.5,'1998-2-2')";
+//string sql = "update Students set Name = 'sakib' where Cgpa = 3.50";
+
+//string sql = "delete from Students where id = 3";
+
+//dataManage.ExecuteCommand(sql);
+
+
+#endregion
+
+
+
+#region Execute Query for Retriving Data
+
+string query = "Select * from Students";
+
+List<Dictionary<string, object>> values = dataManage.ExecuteQuery(query);
+
+foreach (Dictionary<string, object> value in values)
+{
+    foreach (string key in value.Keys)
+    {
+        Console.Write($" {key} : {value[key]}   ");
+    }
+    Console.WriteLine();
+}
+
+#endregion
