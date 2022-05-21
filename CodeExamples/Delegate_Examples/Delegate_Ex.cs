e96ba1d6650bf88fa4e22e1a2a2400cfd5708714
sajjad0057 <sajjadhossain0057@gmail.com>
2022-05-22 02:33:17 +0600

    delegate Example - 1

---
+++ CodeExamples/Delegate_Examples/Delegate_Ex.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Examples
+{
+    public class Delegate_Ex
+    {
+        public delegate int SomeMethod(int a, int b);
+    }
+}
---
+++ CodeExamples/Delegate_Examples/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using  Delegate_Examples;
+
+
+
+//// here , Delegate_Ex.SomeMethod passes just type of y . 
+
+Delegate_Ex.SomeMethod y = Sum;
+
+int res = y(2,3);
+Console.WriteLine($"Result : {res}");
+
+int Sum(int a, int b)
+{
+    return a + b;
+}
