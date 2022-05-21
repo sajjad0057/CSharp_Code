bc47cbe63b7ab0bc13523016f63db5bf5bbe6ef5
sajjad0057 <sajjadhossain0057@gmail.com>
2022-05-22 03:05:01 +0600

    Another Examples of delegates in c#

--- CodeExamples/Delegate_Examples/BubbleSort.cs
+++ CodeExamples/Delegate_Examples/BubbleSort.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Delegate_Examples
 {
-    public class BubbleSort
+    public class BubbleSort<T>
     {
+        public delegate int Compare(T x, T y);
+        public static void Sort(T[] items, Compare compare)
+        {
+            for (int i = 0; i <= items.Length-2; i++)
+            {
+                for (int j = 0; j<= items.Length - 2; j++)
+                {
+                    if (compare(items[j] , items[j + 1]) == -1)
+                    {
+                        var temp = items[j + 1];
+                        items[j + 1] = items[j];
+                        items[j] = temp;
+                    }
+                }
+            }
+        }
     }
 }
--- CodeExamples/Delegate_Examples/Program.cs
+++ CodeExamples/Delegate_Examples/Program.cs
@@ -14,3 +14,25 @@ int Sum(int a, int b)
 {
     return a + b;
 }
+
+int[] arr = new int[] { 1,3, 2, 8,  9, 7, 10, 6, 5 };
+
+BubbleSort<int>.Sort(arr, CompareValue);
+
+
+int CompareValue(int a , int b)
+{
+    if (a > b)
+        return -1;
+    else if (a < b)
+        return 1;
+    return 0;
+}
+
+
+Console.WriteLine("Sorting Result : ");
+
+foreach (int a in arr)
+{
+    Console.Write($"{a} , ");
+}
