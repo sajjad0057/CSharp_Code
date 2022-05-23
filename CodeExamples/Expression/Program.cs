fc1be11e62cdcc9b70ba63256de212c48e5be15d
sajjad0057 <sajjadhossain0057@gmail.com>
2022-05-23 20:21:36 +0600

    touch Expression in C#

---
+++ CodeExamples/Expression/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+/*
+                              For Knowing More About Expression follow this link -
+
+                              ***  https://tyrrrz.me/blog/expression-trees ***
+ 
+*/
+
+using System.Linq.Expressions;
+
+Expression<Func<int, int, int>> sum = (a, b) => a + b;
+
+Func<int,int,int> a = sum.Compile();
+
+int r = a(1, 3);
+Console.WriteLine(r);
