28be270b68602ceca745e0431f76aed2ccf43baf
sajjad0057 <sajjadhossain0057@gmail.com>
2022-05-23 16:44:20 +0600

    Practice Lambda_Method/Expressions

---
+++ CodeExamples/Lambda_Method/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+/*
+ * 
+int Sum(int a, int b)
+{
+    return a + b;
+} 
+
+*/
+
+//// Eqivalent Lambda Method :
+
+
+
+int Sum(int a, int b) => a + b;
+
+
+int res = Sum(5,6);
+
+
+
+
+void Print(int a) => Console.WriteLine(a);
+
+
+////Console.WriteLine(res);
+
+Print(res);
