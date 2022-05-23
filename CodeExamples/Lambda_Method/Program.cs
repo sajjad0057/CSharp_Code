185e6f041a9c257b55da3274ef952c6482386b83
sajjad0057 <sajjadhossain0057@gmail.com>
2022-05-23 19:50:16 +0600

    Practice lambda method / anonymous function

--- CodeExamples/Lambda_Method/Program.cs
+++ CodeExamples/Lambda_Method/Program.cs
@@ -27,3 +27,24 @@ void Print(int a) => Console.WriteLine(a);
 ////Console.WriteLine(res);
 
 Print(res);
+
+
+
+//// Declareing Anonymous Function need to delegate / Func<> / Action<>
+
+
+Func<int, int> Square = x => x * x;
+
+// here : [type]  [variable_name]  = [Anonymous Function]
+
+Console.WriteLine(Square(5));
+
+
+Action<int> test=(int a)=>
+{
+    Console.WriteLine($"{a}+1 = {a+1}");
+    Console.WriteLine($"{a}+2 = {a+2}");
+
+};
+
+test(3);
