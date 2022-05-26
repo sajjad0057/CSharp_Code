22f7eb725972ea18a50befaf4ce397e008c64e9b
sajjad0057 <sajjadhossain0057@gmail.com>
2022-05-27 02:04:54 +0600

    practice Thread Less Program / Code

---
+++ CodeExamples/Threading/PrimeGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    public class PrimeGenerator
+    {
+        public List<int> Generate(int start , int end)
+        {   
+            List<int> primes = new List<int>();
+
+            for(int i = start; i <= end; i++)
+            {
+                if (_isPrime(i))
+                {
+                    primes.Add(i);
+                }
+            }
+
+            return primes;
+        }
+
+        
+        private bool _isPrime(int x)
+        {
+            if( x == 0 || x == 1 )
+                return false;
+
+            for(int i = 2; i < Math.Sqrt(x); i++)
+            {
+                if(x % i == 0)
+                    return false;
+            }
+
+            return true;
+
+        }
+
+
+
+    }
+}
---
+++ CodeExamples/Threading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using Threading;
+
+
+/*
+Provides a set of methods and properties that you can use to accurately measure
+elapsed time. 
+*/
+
+Stopwatch stopwatch = new Stopwatch();
+
+
+stopwatch.Start();
+
+PrimeGenerator primeGernerator = new PrimeGenerator();
+
+var r1 = primeGernerator.Generate(0, 10000000);
+
+var r2 = primeGernerator.Generate(10000, 10000000);
+
+
+stopwatch.Stop();
+
+Console.WriteLine($"Taking Time : {stopwatch.Elapsed}");
