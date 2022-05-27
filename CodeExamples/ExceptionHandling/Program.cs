0f834305bdbd25190f83f8785094fbee2fbcba42
sajjad0057 <sajjadhossain0057@gmail.com>
2022-05-27 06:04:19 +0600

    Basic Exception Handling - 1

---
+++ CodeExamples/ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿
+Console.WriteLine("Practice Exception Handling : ");
+Console.WriteLine("---------------------------------------------------------------------");
+
+
+
+
+int x = 4;
+
+int y = 0;
+
+
+//try
+//{
+//    int z = x / y;
+//}
+//catch (Exception e)
+//{
+//    Console.WriteLine(e.Message);
+//    Console.WriteLine("Divided By Zero Does Not Possible -");
+//}
+
+
+
+
+try
+{
+    var z =  x / y ;
+
+    Console.WriteLine($"{x} / {y} = {z} ");
+
+}catch(DivideByZeroException e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine("Divided By Zero Does Not Possible -");
+}
+catch(Exception e)
+{
+    Console.WriteLine($"An Exception Occur : {e.Message} ");
+};
