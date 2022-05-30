6cd9b575bbc12f9068966adb89d703c9a1767951
sajjad0057 <sajjadhossain0057@gmail.com>
2022-05-31 02:36:44 +0600

    using tuple check multiple value in new switch expressio in switch expression

--- CodeExamples/switch_statement/Program.cs
+++ CodeExamples/switch_statement/Program.cs
@@ -50,8 +50,26 @@ static bool CheckIfCanWalkIntoBank4(Bank bank, bool isVip)
     {
         BankBranchStatus.Open => true,
         BankBranchStatus.Closed => false,
-        BankBranchStatus.VIPCustomersOnly when isVip => true,   // check some new condition or case using when keyword ,
+        BankBranchStatus.VIPCustomersOnly when isVip => true,   //// check some new condition or case using when keyword ,
         BankBranchStatus.VIPCustomersOnly when !isVip => false,
         _ => false
+    };
+}
+
+
+/// <summary>
+/// using tuple  check multiple value in new switch expression
+/// </summary>
+
+
+static bool CheckIfCanWalkIntoBank5(Bank bank, bool isVip)
+{
+    return (bank.Status, isVip) switch
+    {
+        (BankBranchStatus.Open, _) => true,   // here _ indicate default value .
+        (BankBranchStatus.Closed, _) => false,
+        (BankBranchStatus.VIPCustomersOnly, true) => true,
+        (BankBranchStatus.VIPCustomersOnly, false) => false,
+        (_, _) => false
     };
 }
