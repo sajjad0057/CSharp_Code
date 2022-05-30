ebcd3b4ced6cfa9ec9d9c68b183ae65b158fe137
sajjad0057 <sajjadhossain0057@gmail.com>
2022-05-31 02:21:54 +0600

    new switch statement/expression code example - 1

---
+++ CodeExamples/switch_statement/Bank.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_statement
+{
+
+    public class Bank
+    {
+        public BankBranchStatus Status { get; set; }
+
+    }
+
+    public enum BankBranchStatus
+    {
+        Open,
+        Closed,
+        VIPCustomersOnly
+    }
+
+}
---
+++ CodeExamples/switch_statement/Program.cs
@@ -0,0 +1,44 @@
+﻿using switch_statement;
+
+Console.WriteLine("Hello, World!");
+
+
+
+/// <summary>
+/// Example of old switch statement : 
+/// </summary>
+static bool CheckIfCanWalkIntoBankSwitch(Bank bank, bool isVip)
+{
+    switch (bank.Status)
+    {
+        case BankBranchStatus.Open:
+            return true;
+        case BankBranchStatus.Closed:   //// here not need to use break ; cz , here return directly 
+            return false;
+        case BankBranchStatus.VIPCustomersOnly:
+            return isVip;
+        default:
+            return false;
+    }
+}
+
+
+
+/// <summary>
+/// Example of New switch statement : 
+/// </summary>
+/// 
+
+static bool CheckIfCanWalkIntoBank(Bank bank,bool isVip)
+{
+    var result = bank.Status switch
+    {
+        BankBranchStatus.Open => true,
+        BankBranchStatus.Closed => false,
+        BankBranchStatus.VIPCustomersOnly => isVip,
+        _ => false,
+
+    };
+
+
+}
