9cf5cdef85be3e0a7f611298a2ae06f885c4c7b8
sajjad0057 <sajjadhossain0057@gmail.com>
2022-06-01 01:40:00 +0600

    practice  access data usign inverse indexing code

---
+++ CodeExamples/Inverse_indexing/Program.cs
@@ -0,0 +1,40 @@
+﻿
+Console.WriteLine("Inverse Indexing used to access data inversly");
+
+
+
+/// <summary>
+/// Normal indexing are Zero (0) Based . 
+/// </summary>
+/// 
+
+Console.WriteLine("Access data using normal indexing - ");
+
+int[] ages = new int[] { 10, 12, 3, 43 , 15, 4, 6, 33 };
+
+
+Console.WriteLine(ages[0]);
+Console.WriteLine(ages[1]);
+Console.WriteLine(ages[2]);
+Console.WriteLine(ages[3]);
+Console.WriteLine(ages[4]);
+Console.WriteLine(ages[5]);
+Console.WriteLine(ages[6]);
+Console.WriteLine(ages[7]);
+
+
+/// <summary>
+/// Normal indexing are One (1) Based . 
+/// </summary>
+/// 
+
+Console.WriteLine("Access data using reverse indexing - ");
+
+Console.WriteLine(ages[^1]);
+Console.WriteLine(ages[^2]);
+Console.WriteLine(ages[^3]);
+Console.WriteLine(ages[^4]);
+Console.WriteLine(ages[^5]);
+Console.WriteLine(ages[^6]);
+Console.WriteLine(ages[^7]);
+Console.WriteLine(ages[^8]);
