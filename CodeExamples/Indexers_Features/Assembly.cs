72c6779f0fd3a76e3fb88b5f46211a6e44552553
sajjad0057 <sajjadhossain0057@gmail.com>
2022-06-01 03:15:36 +0600

    Another Examples of Indexer_Feature

---
+++ CodeExamples/Indexers_Features/Assembly.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers_Features
+{
+    public class Assembly
+    {
+        public List<Person> People = new List<Person>();
+        public Person this[string name]
+        {
+            get
+            {
+               return (from p in People where p.Name == name select p).FirstOrDefault();
+            }
+            set
+            {
+                int index =  People.FindIndex(x => x.Name == name);
+                People[index] = value;
+
+            }
+        }
+    }
+}
--- CodeExamples/Indexers_Features/Program.cs
+++ CodeExamples/Indexers_Features/Program.cs
@@ -1,17 +1,24 @@
 ﻿
 using Indexers_Features;
 
+
+
 Keyboard keyboard = new Keyboard();
 
-keyboard.Keys.Add(new Key('A'));
-keyboard.Keys.Add(new Key('B'));
+keyboard.Keys.Add(new Key(97));
+keyboard.Keys.Add(new Key(98));
 keyboard.Keys.Add(new Key('C'));
 keyboard.Keys.Add(new Key('D'));
-keyboard.Keys.Add(new Key('E'));
-
-keyboard.Keys.Add(new Key(96));
 
 
-Console.WriteLine($"{keyboard['C'].KeyCharacter}");
-Console.WriteLine($"{keyboard['E'].KeyCharacter}");
-Console.WriteLine($"{keyboard[96].AsciiValue}");
+Console.WriteLine(keyboard[97].AsciiValue);
+Console.WriteLine(keyboard['C'].KeyCharacter);
+
+
+Assembly assembly = new Assembly();
+
+assembly.People.Add(new Person { Name = "sajjad hossain", age = 24 });
+assembly.People.Add(new Person { Name = "Zahan", age = 23 });
+
+Console.WriteLine(assembly["sajjad hossain"].Name);
+Console.WriteLine(assembly["Zahan"].Name);
