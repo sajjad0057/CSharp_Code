16d5bef387c4feb7ef8c92bed2cf23d93df3d87b
sajjad0057 <sajjadhossain0057@gmail.com>
2022-06-04 17:52:55 +0600

    Code examples of violate open close design principles

---
+++ CodeExamples/SOLID_Principles/Geometry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles
+{
+
+    /*
+    This class / method Examples are violate open close principles 
+    */
+    public class Geometry
+    {
+        public double CalCulateArea(string shapeType , object[] parameters)
+        {
+            if (shapeType == "circle")
+                return Math.PI * (double)parameters[0] * (double)parameters[0];
+            else if (shapeType == "square")
+                return (double)parameters[0] * (double)parameters[0];
+            else if (shapeType == "rectangle")
+                return (double)parameters[0] * (double)parameters[1];
+            else
+                return 0;
+
+        }
+    }
+}
--- CodeExamples/SOLID_Principles/Membership.cs
+++ CodeExamples/SOLID_Principles/Membership.cs
@@ -8,9 +8,12 @@ namespace SOLID_Principles
 {
     public class Membership
     {
+        /*
+         According to SRP , A Class Can contain just Same responsible related code ... 
+         */
         public void Singup(string username, string email, string password)
         {
-            //  implimentation 1, check username / email is exists or not .
+            // Implimentation 1, check username / email is exists or not .
             // 2. Create a new user with username and password , email - > Data Operations 
             /* By the Condition of SRP , this class cannot contain  database related code or send confirmation email code . 
              * coz it is not related responsibility of member class 
