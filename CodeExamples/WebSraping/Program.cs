0b579b526dfe60937982963bfe1f55d783884b2c
sajjad0057 <sajjadhossain0057@gmail.com>
2022-12-07 22:53:51 +0600

    updated code

--- CodeExamples/WebSraping/Program.cs
+++ CodeExamples/WebSraping/Program.cs
@@ -1,4 +1,5 @@
 ﻿using HtmlAgilityPack;
+using System.Text.RegularExpressions;
 
 Console.WriteLine("Hello, World!");
 
@@ -9,23 +10,64 @@ HtmlDocument document = web.Load("https://www.dse.com.bd/latest_share_price_scro
 
 //var nodes = document.DocumentNode.SelectNodes("//tr/td").Select(x => x.InnerText).ToArray();
 
-var table = document.DocumentNode.SelectNodes("//div[@class='table-responsive inner-scroll']/table/tbody/tr");
+var tables = document.DocumentNode.SelectNodes("//table//tr");
 
-foreach (var row in table)
+foreach (var node in tables)
 {
-    Console.WriteLine($"{row.InnerText.Trim()}");
-    Console.WriteLine("-----------------------------------");
+    //var id = node.SelectSingleNode($"td[{2}]").InnerText;
+    if (node.SelectSingleNode($"td[{2}]") != null)
+    {
+        var id = node.SelectSingleNode($"td[{2}]").InnerText;
+
+        Console.WriteLine(id.Trim());
+
+    }
+
+    if (node.SelectSingleNode($"td[{3}]") != null)
+    {
+        var id = node.SelectSingleNode($"td[{3}]").InnerText;
+        if (id.Length <= 10)
+        {
+            Console.WriteLine(id.Trim());
+        }
+    }
 }
 
-var table2 = document.DocumentNode.SelectNodes("//div[@class='table-responsive inner-scroll']/table/tr");
 
-foreach(var x in table2)
-{
-    Console.WriteLine($"{x.InnerText}");
-    Console.WriteLine("-----------------------------------");
-}
 
-Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var tables = document.DocumentNode.SelectSingleNode("//table");
+
+//foreach (var node in tables.SelectNodes("//tr"))
+//{
+//    string tradingCode = ParsedValues(node, 2);
+//    string latestPrice = ParsedValues(node, 3);
+//    if (latestPrice.Length >= 1 && latestPrice.Length <= 8)
+//    {
+//        Console.WriteLine(tradingCode);
+//    }
+//}
+
+
+//string ParsedValues(HtmlNode node, int index)
+//{
+//    var id = node.SelectSingleNode($"td[{index}]") ==
+//        null ? "" : node.SelectSingleNode($"td[{index}]").InnerText;
+//    return Regex.Replace(id, @"\s", "");
+//}
 
 
 
