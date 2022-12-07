8aef5463965f1b7129d33a05a0082deb43d933b5
sajjad0057 <sajjadhossain0057@gmail.com>
2022-12-07 17:13:34 +0600

    successfully get table data by scraping web

---
+++ CodeExamples/WebSraping/Program.cs
@@ -0,0 +1,29 @@
+﻿using HtmlAgilityPack;
+
+Console.WriteLine("Hello, World!");
+
+
+HtmlWeb web = new HtmlWeb();
+
+HtmlDocument document = web.Load("https://www.dse.com.bd/latest_share_price_scroll_l.php");
+
+//var nodes = document.DocumentNode.SelectNodes("//tr/td").Select(x => x.InnerText).ToArray();
+
+var table = document.DocumentNode.SelectNodes("//div[@class='table-responsive inner-scroll']/table/tbody/tr/td");
+
+foreach (var row in table)
+{
+    Console.WriteLine($"{row.InnerText.Trim()}");
+}
+
+var table2 = document.DocumentNode.SelectNodes("//div[@class='table-responsive inner-scroll']/table/tr/td");
+
+foreach(var x in table2)
+{
+    Console.WriteLine($"{x.InnerText.Trim()}");
+}
+
+Console.WriteLine();
+
+
+
