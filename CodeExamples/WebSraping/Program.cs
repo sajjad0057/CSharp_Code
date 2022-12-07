1411bdd18eb0e56fbebf0660c82dd171f9dcdc68
sajjad0057 <sajjadhossain0057@gmail.com>
2022-12-07 21:36:52 +0600

    Successfully fetch data from dse

--- CodeExamples/WebSraping/Program.cs
+++ CodeExamples/WebSraping/Program.cs
@@ -9,18 +9,20 @@ HtmlDocument document = web.Load("https://www.dse.com.bd/latest_share_price_scro
 
 //var nodes = document.DocumentNode.SelectNodes("//tr/td").Select(x => x.InnerText).ToArray();
 
-var table = document.DocumentNode.SelectNodes("//div[@class='table-responsive inner-scroll']/table/tbody/tr/td");
+var table = document.DocumentNode.SelectNodes("//div[@class='table-responsive inner-scroll']/table/tbody/tr");
 
 foreach (var row in table)
 {
     Console.WriteLine($"{row.InnerText.Trim()}");
+    Console.WriteLine("-----------------------------------");
 }
 
-var table2 = document.DocumentNode.SelectNodes("//div[@class='table-responsive inner-scroll']/table/tr/td");
+var table2 = document.DocumentNode.SelectNodes("//div[@class='table-responsive inner-scroll']/table/tr");
 
 foreach(var x in table2)
 {
-    Console.WriteLine($"{x.InnerText.Trim()}");
+    Console.WriteLine($"{x.InnerText}");
+    Console.WriteLine("-----------------------------------");
 }
 
 Console.WriteLine();
