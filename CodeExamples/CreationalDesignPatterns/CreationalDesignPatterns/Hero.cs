5918094747311601d3dc9b4b7c574a7218355d3a
sajjad0057 <sajjadhossain0057@gmail.com>
2023-01-01 16:50:50 +0600

    Examples of prototype design patterns

---
+++ CodeExamples/CreationalDesignPatterns/CreationalDesignPatterns/Hero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalDesignPatterns.CreationalDesignPatterns
+{
+    /// <summary>
+    /// Example for Prototype design pattern
+    /// </summary>
+    public class Hero : ICloneable
+    {
+        public string? Name { get; set; }
+        public string? Color { get; set; }
+        public double? Speed { get; set; }
+        public int? Power { get; set; }
+
+        public object Clone()     //// Implement from ICloneable Interface
+        {
+            return Copy();
+        }
+
+        public Hero Copy()
+        {
+            return new Hero { Name = Name, Color = Color, Speed = Speed, Power = Power };
+        }
+    }
+}
