7b0a77038fdceac6da667f52bc7570b2829c5823
sajjad0057 <sajjadhossain0057@gmail.com>
2023-01-09 15:40:22 +0600

    Convert Json String to ListOfObject

--- CodeExamples/ManageKeyCountString/Program.cs
+++ CodeExamples/ManageKeyCountString/Program.cs
@@ -1,11 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ManageKeyCountString;
 using System.Text.Json;
 
 string keyCounts = "[{\"Key\":\"Others\",\"Counts\":10},{ \"Key\":\"s\",\"Counts\":1},{ \"Key\":\"o\",\"Counts\":2},{ \"Key\":\"n\",\"Counts\":4},{ \"Key\":\"g\",\"Counts\":1},{ \"Key\":\"j\",\"Counts\":5},{ \"Key\":\"h\",\"Counts\":3},{ \"Key\":\"k\",\"Counts\":3},{ \"Key\":\"l\",\"Counts\":1},{ \"Key\":\"i\",\"Counts\":1},{ \"Key\":\"b\",\"Counts\":1},{ \"Key\":\"v\",\"Counts\":1},{ \"Key\":\"c\",\"Counts\":1},{ \"Key\":\"u\",\"Counts\":2}]";
 
-var x = keyCounts.Trim('[', ']').Split(',').ToList();
+//Console.WriteLine(keyCounts);
 
-foreach (var item in x)
+var ListOfKeyCounts = JsonSerializer.Deserialize<IList<KeysCount>>(keyCounts);
+
+foreach(var item in ListOfKeyCounts)
 {
-    Console.WriteLine(item);
+    Console.WriteLine($"{item.Key} : {item.Counts}");
 }
