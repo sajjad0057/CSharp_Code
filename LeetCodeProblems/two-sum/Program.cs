388334de1d3f2a576641e33b88fc08bb5b21d8ab
sajjad0057 <sajjadhossain0057@gmail.com>
2023-02-19 00:03:43 +0600

    Sumbit 1st problems on Leetcode: Two-Sum

---
+++ LeetCodeProblems/two-sum/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] TwoSum(int[] nums, int target)
+{
+    for(int i = 0; i < nums.Length; i++)
+    {
+        for(int j =i+1; j < nums.Length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+                return new int[] { i, j };
+        }
+    }
+
+    return new int[] {};
+}
+
+
+var x = TwoSum(new int[] { 2, 5, 5, 11 }, 10);
+
+foreach(int i in x)
+{
+    Console.WriteLine(i);
+}
+
+
+
+
+
+
+
