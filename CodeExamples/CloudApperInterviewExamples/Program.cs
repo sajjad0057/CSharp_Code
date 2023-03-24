7014c5f59eda0eb3c310e8c861f316357e8508e3
sajjad0057 <sajjadhossain0057@gmail.com>
2023-03-24 17:12:40 +0600

    Practice Interview question

---
+++ CodeExamples/CloudApperInterviewExamples/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+int sum(int a, int b, int c= 0)
+{
+    return a + b + c;
+}
+
+Console.WriteLine(sum(1,3,4));
+
+
+
+a();
+void a()
+{
+    try
+    {
+        b();
+    }catch(Exception e) 
+    {
+        throw e;   
+    }
+}
+        
+
+void b()
+{
+    try
+    {
+        int a = 1;
+        int x = 0;
+
+        int y = a / x;
+    }
+    catch (Exception e) 
+    {
+        throw e;
+    }
+}
