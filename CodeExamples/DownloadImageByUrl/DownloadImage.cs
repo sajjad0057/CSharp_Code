d01652257f2b71098467d499b5af86fe8a7b477e
sajjad0057 <sajjadhossain0057@gmail.com>
2023-03-25 00:47:02 +0600

    download image by url

---
+++ CodeExamples/DownloadImageByUrl/DownloadImage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadImageByUrl
+{
+    public class DownloadImage
+    {
+        private readonly string _ImageUrl;
+
+        public DownloadImage(string imageUrl)
+        {
+            _ImageUrl= imageUrl;
+        }
+
+        public void download()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    var imageBytes = client.DownloadData(_ImageUrl);
+                    Console.WriteLine(imageBytes);
+                    var rootPath = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.Parent;
+                    var folderPath = Path.Combine(rootPath.FullName,"images");
+                    if (!Directory.Exists(folderPath))
+                    {
+                        Directory.CreateDirectory(folderPath);
+                    }
+                    Console.WriteLine(folderPath);
+                    File.WriteAllBytes($"{folderPath}\\img.jpg", imageBytes);
+                    Console.WriteLine("Image downloaded !");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error Occurred : {ex.Message}");
+            }
+        }
+
+    }
+}
---
+++ CodeExamples/DownloadImageByUrl/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using DownloadImageByUrl;
+
+string imgUrl = @"https://3.bp.blogspot.com/-5ssCcG_F1UM/VzW7LKgVJkI/AAAAAAAAB18/gnyJ0K00NTU57MafNK-xeOO_CD5BBDCIwCLcB/s1600/How%2Bto%2Bdownload%2Bimage%2Bfrom%2Burl%2Bwith%2BPHP.png";
+
+
+DownloadImage downloadImage = new DownloadImage(imgUrl);
+
+downloadImage.download();
