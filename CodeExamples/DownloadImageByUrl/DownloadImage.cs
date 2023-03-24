a7cd221c5e3fa76762628cd511ff145492488c0a
sajjad0057 <sajjadhossain0057@gmail.com>
2023-03-25 01:56:17 +0600

    dowload multiple image using thread pool

--- CodeExamples/DownloadImageByUrl/DownloadImage.cs
+++ CodeExamples/DownloadImageByUrl/DownloadImage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -39,6 +40,53 @@ namespace DownloadImageByUrl
             {
                 Console.WriteLine($"Error Occurred : {ex.Message}");
             }
+        }
+
+
+        public async Task DownloadImagesAsync(IEnumerable<string> imageUrls)
+        {
+            var httpClient = new HttpClient();
+            var tasks = new List<Task>();
+            using var throttler = new SemaphoreSlim(3);
+            foreach (var url in imageUrls)
+            {
+                await throttler.WaitAsync();
+                tasks.Add(Task.Run(async () =>
+                {
+                    try
+                    {
+                        var response = await httpClient.GetAsync(url);
+                        response.EnsureSuccessStatusCode();
+                        var bytes = await response.Content.ReadAsByteArrayAsync();
+
+                        var rootPath = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.Parent;
+                        var folderPath = Path.Combine(rootPath.FullName, "images");
+                        if (!Directory.Exists(folderPath))
+                        {
+                            Directory.CreateDirectory(folderPath);
+                        }
+
+                        var imgName = Guid.NewGuid().ToString()+".jpg";
+
+                        Console.WriteLine($"image name : {imgName}");
+
+                        await Task.Delay(3000);
+
+                        Console.WriteLine($"image name> : {imgName}");
+
+                        File.WriteAllBytes($"{folderPath}\\{imgName}", bytes);
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    finally
+                    {
+                        throttler.Release();
+                    }
+                }));
+            }
+            await Task.WhenAll(tasks);
         }
 
     }
--- CodeExamples/DownloadImageByUrl/Program.cs
+++ CodeExamples/DownloadImageByUrl/Program.cs
@@ -6,4 +6,16 @@ string imgUrl = @"https://3.bp.blogspot.com/-5ssCcG_F1UM/VzW7LKgVJkI/AAAAAAAAB18
 
 DownloadImage downloadImage = new DownloadImage(imgUrl);
 
-downloadImage.download();
+//downloadImage.download();
+
+IEnumerable<string> imgUrls = new List<string>()
+{
+ @"https://3.bp.blogspot.com/-5ssCcG_F1UM/VzW7LKgVJkI/AAAAAAAAB18/gnyJ0K00NTU57MafNK-xeOO_CD5BBDCIwCLcB/s1600/How%2Bto%2Bdownload%2Bimage%2Bfrom%2Burl%2Bwith%2BPHP.png",
+ @"https://media.wired.com/photos/5b8999943667562d3024c321/master/w_2560%2Cc_limit/trash2-01.jpg",
+ @"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR807WzVyUt4KpAm3ngJpl1UlkEz6ehBaKAzAHY8_rWKZrDrlAX0_4Z-j42uuKdOM3JCZg&usqp=CAU"
+};
+
+
+await downloadImage.DownloadImagesAsync(imgUrls);
+
+
