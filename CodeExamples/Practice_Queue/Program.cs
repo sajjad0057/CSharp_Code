2f822abcaec5933f5a21a7b510077a9bfded6569
sajjad0057 <sajjadhossain0057@gmail.com>
2023-03-27 14:29:51 +0600

    practice more with GroupBy linq

--- CodeExamples/Practice_Queue/Program.cs
+++ CodeExamples/Practice_Queue/Program.cs
@@ -4,30 +4,30 @@ using System.Collections;
 Console.WriteLine("Hello, World!");
 
 
-Queue<string> myQ = new Queue<string>();
-myQ.Enqueue("1");
-myQ.Enqueue("2");
-myQ.Enqueue("3");
-myQ.Enqueue("4");
-myQ.Enqueue("5");
-myQ.Enqueue("6");
-myQ.Enqueue("7");
-myQ.Enqueue("8");
-myQ.Enqueue("9");
-myQ.Enqueue("10");
-myQ.Enqueue("11");
-myQ.Enqueue("12");
+//Queue<string> myQ = new Queue<string>();
+//myQ.Enqueue("1");
+//myQ.Enqueue("2");
+//myQ.Enqueue("3");
+//myQ.Enqueue("4");
+//myQ.Enqueue("5");
+//myQ.Enqueue("6");
+//myQ.Enqueue("7");
+//myQ.Enqueue("8");
+//myQ.Enqueue("9");
+//myQ.Enqueue("10");
+//myQ.Enqueue("11");
+//myQ.Enqueue("12");
 
-while (myQ.Count > 0)
-{
-    Console.WriteLine(myQ.Dequeue());
-}
-
-
-IEnumerable<string> list = new List<string>() { "a","b","c","d","e","f","g"};
+//while (myQ.Count > 0)
+//{
+//    Console.WriteLine(myQ.Dequeue());
+//}
 
 
-Queue<IList<string>> q = new Queue<IList<string>>();
+//IEnumerable<string> list = new List<string>() { "a","b","c","d","e","f","g"};
+
+
+//Queue<IList<string>> q = new Queue<IList<string>>();
 
 
 //for(int i = 0; i < list.Count(); i = +3)
@@ -53,6 +53,7 @@ Console.WriteLine();
 
 
 List<string> myList = new List<string> { "apple", "banana", "orange", "grape", "peach", "pear", "kiwi" };
+
 int batchSize = 3;
 
 Queue<List<string>> batchQueue = new Queue<List<string>>();
@@ -64,5 +65,22 @@ myList.Select((value, index) => new { Index = index, Value = value })
       .ForEach(batch => batchQueue.Enqueue(batch));
 
 
-Console.WriteLine();
+
+var select = myList.Select((value, index) => new { Index = index, Value = value });
+
+var group = select.GroupBy(x=> testValue(x.Index)).ToList();
+
+var select2 = group.Select(x => x.Select(v => v.Value).ToList()).ToList();
+
+
+int testValue(int a)
+{
+    Console.WriteLine(a/4);
+
+    return a / 4;
+}
+
+//Func<int, int> test = testValue;
+
+Console.WriteLine($"select : {select}");
 
