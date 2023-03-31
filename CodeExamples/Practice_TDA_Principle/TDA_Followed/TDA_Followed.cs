1de88c947c4b7ea2724f16e8b2fa6dd4b25c501a
sajjad0057 <sajjadhossain0057@gmail.com>
2023-04-01 02:04:43 +0600

    practice TDA_Principle

---
+++ CodeExamples/Practice_TDA_Principle/TDA_Followed/TDA_Followed.cs
@@ -0,0 +1,59 @@
+﻿
+/*
+use the Tell, Don't Ask principle. So we change our code to shift the entire logic into the CostCalculator
+class and pass the required data to it. It calculates the net price and based on results, it decides whether
+or not it needs to make a further call to the ApplyAccessories() method. Our code now changes to:
+*/
+
+namespace Practice_TDA_Principle.TDA_Followed;
+
+public class TDA_Followed
+{
+    public void CalculatePrice(Vehicle vehicle, CostCalculator costCalculator, Int32 discount)
+    {
+        costCalculator.NetCalculation(vehicle, discount);
+    }
+}
+
+
+public class CostCalculator
+{
+    public int BasePrice { get; set; }
+    public void NetCalculation(Vehicle vehicle, int discount)
+    {
+        var netPrice = discount > 5 ? vehicle.TotalPrice - 10000 : vehicle.TotalPrice - 20000;
+
+        if(netPrice > this.BasePrice)
+        {
+            this.ApplyAccessories();
+        }
+        else
+        {
+            //// logical code
+        }
+    }
+
+
+    private void ApplyAccessories()
+    {
+        //// logical code 
+    }
+}
+
+
+public class Vehicle
+{
+    public int TotalPrice { get; set; }
+}
+
+
+
+
+/*
+So in this case, the entire calculation logic, along with the logic to add accessories,
+is inside the CostCalculator class. We simply provide the required data as input to this class.
+Further, the ApplyAccessories method is now converted into a private method and the client code 
+has no idea of the cost calculation business logic. The client code will simply pass the data to 
+the CostCalculator and the rest will be done by its NetCalculation method. In other words, we 
+aretelling the CostCalculator instance to perform the calculations,rather than asking anything from it.
+*/
---
+++ CodeExamples/Practice_TDA_Principle/TDA_Violation/TDA_Violation.cs
@@ -0,0 +1,50 @@
+﻿namespace Practice_TDA_Principle.TDA_Violation;
+
+public class TDA_Violation
+{
+    public void CalculatePrice(Vehicle vehicle, CostCalculaor costCalculator, int discount)
+    {
+        var netPrice = discount > 5 ? vehicle.TotalPrice = 5000 : vehicle.TotalPrice - 3000;
+
+        if(netPrice > costCalculator.BasePrice)
+        {
+            costCalculator.ApplyAccessories();
+        }
+        else
+        {
+            //// Accessories cannot be provided
+        }
+    }
+}
+
+
+public class CostCalculaor
+{
+    public int BasePrice { get; set; }
+
+    public void ApplyAccessories()
+    {
+        //// logical code
+    }
+}
+
+
+public class Vehicle
+{
+    public int TotalPrice { get; set; }
+}
+
+
+/*
+We check for the discount applicable on the vehicle. If it is greater than 5%, we subtract 5000 from the actual price else 3000, as a discount. We also have a net price price defined for the vehicle. After applying the discount, if the net price is more than this base price, we can further provide free accessories else we do not.
+
+This type of code will have the following issues:
+
+1. If we have the same type of logic in other areas of the application then changes in the logic 
+   will require changes in all the locations.
+ 
+2. The client code is now aware of the business logic to calculate the cost of the vehicle.
+ 
+3. Some of the member functions/properties are unnecessarily exposed as public members to the client code.
+
+*/
