36bfa5015673c3e39bb27f632fc04777cba694c6
sajjad0057 <sajjadhossain0057@gmail.com>
2023-05-22 13:36:37 +0600

    Practice in , out and ref parameter modifier

---
+++ CodeExamples/in_out_ref_modifiers/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace in_out_ref_modifiers
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CodeExamples/in_out_ref_modifiers/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using in_out_ref_modifiers;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+int x = 5;
+
+int func(in int x)
+{
+    return x;
+}
+
+Console.WriteLine($"Example for in modifier : {func(in x)}");
+
+
+int m;
+
+Console.WriteLine($"Example of out modifier : {func1(out m)}");
+
+int func1(out int m)
+{
+    m = 5*5;
+
+    return m;
+}
+
+Console.WriteLine();
+
+
+Person person1 = new Person
+{
+    Id = 1,
+    Name = "Foo",
+};
+
+Console.WriteLine(JsonSerializer.Serialize(person1));
+
+void func2(ref Person m)
+{
+    m.Id = 2;
+    m.Name = "Bar";
+}
+
+func2(ref person1);
+
+Console.WriteLine(JsonSerializer.Serialize(person1));
+
+Console.WriteLine();
