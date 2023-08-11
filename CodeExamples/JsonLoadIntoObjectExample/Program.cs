f7535f44739a3c62ee0e1e57cd00bcd77027649a
sajjad0057 <sajjadhossain0057@gmail.com>
2023-08-11 15:43:49 +0600

    Practice serilize and deserialie object

---
+++ CodeExamples/JsonLoadIntoObjectExample/Program.cs
@@ -0,0 +1,10 @@
+﻿using JsonLoadIntoObjectExample;
+using System.Text.Json;
+
+string jsonData = "{\r\n    \"Address\": {\r\n        \"Street\": \"456 Elm Street\",\r\n        \"City\": \"Somewhereville\",\r\n        \"Country\": \"Canada\"\r\n    },\r\n    \"Person\": {\r\n        \"Name\": \"Alice Smith\",\r\n        \"Age\": 28,\r\n        \"Address\": {\r\n            \"Street\": \"123 Oak Avenue\",\r\n            \"City\": \"Anytown\",\r\n            \"Country\": \"USA\"\r\n        }\r\n    }\r\n}";
+
+var personDetails = JsonSerializer.Deserialize<PersonDetails>(jsonData);
+
+Console.WriteLine($"{JsonSerializer.Serialize(personDetails)}");
+
+Console.WriteLine();
---
+++ CodeExamples/JsonLoadIntoObjectExample/TestClasses.cs
@@ -0,0 +1,24 @@
+﻿namespace JsonLoadIntoObjectExample;
+
+public class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+}
+
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Address Address { get; set; }
+}
+
+
+
+public class PersonDetails
+{
+    public Address Address { get; set; }
+    public Person Person { get; set; }
+}
