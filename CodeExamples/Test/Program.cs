5f4983dae742a000cfcd1f58709856874091bab3
sajjad0057 <sajjadhossain0057@gmail.com>
2023-12-04 11:39:18 +0600

    Practice ignore property serialization using newtonsoft json

--- CodeExamples/Test/Program.cs
+++ CodeExamples/Test/Program.cs
@@ -1,7 +1,17 @@
-﻿var x = "ss";
+﻿
+
+List<Dictionary<string, string>> dictList1 = new();
+
+List<Dictionary<string, string>> dictList2 = new List<Dictionary<string, string>>()
+{
+    new Dictionary<string, string>
+    {
+        {"name","name1" },
+        {"name","name1" }
+
+    },
+};
 
 
-if(x == "ss")
-    Console.WriteLine(x+1);
-if(x == "ss")
-    Console.WriteLine(x+2);
+Console.WriteLine();
+
