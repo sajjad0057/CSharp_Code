ecc208cf0e8c90c08e36abd73866426ef800deba
sajjad0057 <sajjadhossain0057@gmail.com>
2025-02-11 17:49:45 +0600

    Adding project for practice c#12 news

---
+++ CodeExamples/CSharp12_New/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace CSharp12_New;
+
+public class Person(string name, string title)
+{
+    public string Name { get; set; } = name;
+    public string Title { get; set; } = title;
+}
---
+++ CodeExamples/CSharp12_New/Program.cs
@@ -0,0 +1,14 @@
+﻿using CSharp12_New;
+
+Console.WriteLine("---------------------c# 12 new--------------------");
+
+Person person = new ("sajjad", "student");
+
+if(person is { Title : "student" })
+{
+    Console.WriteLine($"Person Name : {person.Name}; Title : {person.Title}");
+}
+else
+{
+    Console.WriteLine("Person Title does not exists");
+}
