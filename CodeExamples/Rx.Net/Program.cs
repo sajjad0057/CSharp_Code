83e519b1807f053444dd088b7567d6668207aef5
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-10-03 15:18:32 +0600

    Practice BehaviourSubject<T> in Rx.Net

---
+++ CodeExamples/Rx.Net/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Reactive.Subjects;
+
+var behavior = new BehaviorSubject<int>(0); // initial value = 0
+
+behavior.Subscribe(x => Console.WriteLine("Subscriber One : " + x));
+behavior.Subscribe(x => Console.WriteLine("Subscriber Two : " + x));
+
+behavior.OnNext(1);
+behavior.OnNext(2);
+behavior.OnNext(3);
