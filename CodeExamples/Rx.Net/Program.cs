2436b8cc0db4d5422a47d33395bc2781da80448c
sajjad0057 <sajjad.ice.pust@gmail.com>
2025-10-03 15:22:31 +0600

    practice (BehaviorSubject + CompositeDisposable)

--- CodeExamples/Rx.Net/Program.cs
+++ CodeExamples/Rx.Net/Program.cs
@@ -1,10 +1,32 @@
-﻿using System.Reactive.Subjects;
+﻿using System.Reactive.Disposables;
+using System.Reactive.Subjects;
 
-var behavior = new BehaviorSubject<int>(0); // initial value = 0
+//var behavior = new BehaviorSubject<int>(0); // initial value = 0
 
-behavior.Subscribe(x => Console.WriteLine("Subscriber One : " + x));
-behavior.Subscribe(x => Console.WriteLine("Subscriber Two : " + x));
+//behavior.Subscribe(x => Console.WriteLine("Subscriber One : " + x));
+//behavior.Subscribe(x => Console.WriteLine("Subscriber Two : " + x));
 
-behavior.OnNext(1);
-behavior.OnNext(2);
-behavior.OnNext(3);
+//behavior.OnNext(1);
+//behavior.OnNext(2);
+//behavior.OnNext(3);
+
+
+var disposables = new CompositeDisposable();
+
+var subject = new BehaviorSubject<int>(0);
+
+var sub1 = subject.Subscribe(x => Console.WriteLine("Sub One : " + x));
+var sub2 = subject.Subscribe(x => Console.WriteLine("Sub Two : " + x));
+
+// সাবস্ক্রিপশনগুলো CompositeDisposable এ রাখা হলো
+disposables.Add(sub1);
+disposables.Add(sub2);
+
+subject.OnNext(1);
+subject.OnNext(2);
+
+// এখন একসাথে সব dispose করে দেওয়া হলো
+disposables.Dispose();
+
+// এখন আর কেউ notification পাবে না
+subject.OnNext(3);
