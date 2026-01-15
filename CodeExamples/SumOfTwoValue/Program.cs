12767928c41d6d073902bb6f9927c3be30b23371
sajjad0057 <sajjadhossain0057@gmail.com>
2026-01-15 18:15:20 +0600

    sum of two value prblm solution

---
+++ CodeExamples/SumOfTwoValue/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using SumOfTwoValue;
+
+var solution = new Solution();
+
+int[] arr = { 2, 3, 1, 5, 6, 10 };
+int target = 9;
+
+var x = solution.TwoSum(arr, target);
+
+Console.WriteLine($"[{x[0]},{x[1]}]");
+
+
+
+
---
+++ CodeExamples/SumOfTwoValue/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace SumOfTwoValue;
+
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            if (map.ContainsKey(complement))
+            {
+                return new int[] { map[complement], i };
+            }
+
+            // Store current number and its index
+            map[nums[i]] = i;
+        }
+
+        // This line will never be reached because exactly one solution exists
+        return new int[0];
+    }
+}
